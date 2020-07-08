f0b715913fc96f617427b57b7eafd06c13432738
Catherine Davis <catherinevtdavis@gmail.com>
2020-07-08 12:35:28 -0600

    initial commit

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENDPOINT: `postgresql://catherinedavis@localhost/bookmarks`,
+  API_KEY: '89cb0cba-b56f-11ea-b3de-0242ac130004$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+}
